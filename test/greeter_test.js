b9382897e8335378be19893e92b57255e5754d28
takashimamorino <does1026@gmail.com>
2021-05-05 02:46:44 +0900

    4.5

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -1,6 +1,6 @@
 const GreeterContract = artifacts.require('Greeter')
 
-contract('Greeter', () => {
+contract('Greeter', (accounts) => {
   it('has been deployed successfully', async () => {
     const greeter = await GreeterContract.deployed()
     assert(greeter, 'contaract was not deployed')
@@ -14,17 +14,49 @@ contract('Greeter', () => {
       assert.equal(actual, expected, 'greeted with Hello, World!')
     })
   })
+
+  describe('owner()', () => {
+    it('returns the address of the owner', async () => {
+      const greeter = await GreeterContract.deployed()
+      const owner = await greeter.owner()
+      assert(owner, 'the current owner')
+    })
+
+    it('matches the address that originally deployed the contract', async () => {
+      const greeter = await GreeterContract.deployed()
+      const owner = await greeter.owner()
+      const expected = accounts[0]
+      assert.equal(owner, expected, 'matches address used to deploy contract')
+    })
+  })
 })
 
-contract('Greeter: update greeting()', () => {
+contract('Greeter: update greeting()', (accounts) => {
   describe('setGreeting(string)', () => {
-    it('sets greeting to passed in string', async () => {
-      const greeter = await GreeterContract.deployed()
-      const expected = 'Hi There!'
+    describe('when message is sent by the owner', () => {
+      it('sets greeting to passed in string', async () => {
+        const greeter = await GreeterContract.deployed()
+        const expected = 'Hi There!'
 
-      await greeter.setGreeting(expected)
-      const actual = await greeter.greet()
-      assert.equal(actual, expected, 'greeting was not updated')
+        await greeter.setGreeting(expected)
+        const actual = await greeter.greet()
+        assert.equal(actual, expected, 'greeting was not updated')
+      })
+    })
+
+    describe('when message is sent by another account', () => {
+      it('does not set the greeting', async () => {
+        const greeter = await GreeterContract.deployed()
+        const expected = await greeter.greet()
+        try {
+          await greeter.setGreeting('Not the owner', {from: accounts[1]})
+        } catch(error) {
+          const errorMessage = 'Ownable: caller is not the owner'
+          assert.equal(error.reason, errorMessage)
+          return
+        }
+        assert(false, 'greeting should not update')
+      })
     })
   })
 })
